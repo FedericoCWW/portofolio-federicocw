f7f70cf10b4871a75e94c900630c35b5dd1fe20e
Federico <133919977+FedericoCWW@users.noreply.github.com>
2023-07-30 23:39:27 -0300

    first-commit

---
+++ static/static.js
@@ -0,0 +1,23 @@
+var li_elems = document.querySelectorAll(".seccion2 .navbar ul li")
+var item_elems = document.querySelectorAll(".item")
+for(var i = 0; i < li_elems.length; i++){
+    li_elems[i].addEventListener("click", function(){
+        li_elems.forEach(function(li){
+            li.classList.remove("active");
+        })
+        this.classList.add("active");
+        var li_value = this.getAttribute("data-li");
+        item_elems.forEach(function(item){
+            item.style.display = "none";
+        })
+        if(li_value == "a1"){
+            document.querySelector("." + li_value).style.display = "block";
+        }
+        else if(li_value == "a2"){
+            document.querySelector("." + li_value).style.display = "block";
+        }
+        else if(li_value == "a3"){
+            document.querySelector("." + li_value).style.display = "block";
+        }
+    });
+}
